8d14c2dcf54766885990163654c549d4490ad667
agarwalshantanu29 <agarwalshantanu29@gmail.com>
2022-11-02 12:54:48 +0530

    chenged

--- Prime.js
+++ Prime.js
@@ -1,11 +1,11 @@
- ## Checking a prime number 
+ //Checking a prime number 
 function Pcheck(number){
- let count==0
-for(let i=1;i<num;i++){
+ let count==0;
+for(let i=1;i<number;i++){
 if(num/i==0){
 count++}}
 if(count==2){
 return true}
-return falsee}
+return fals}
 
 let check=Pcheck(13)
