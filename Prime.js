41b8e7e1d77c3a1897c5a0615bb0848441a0025f
DishaGup <guptamansi201@gmail.com>
2022-11-02 11:37:10 +0530

    prime check

---
+++ Prime.js
@@ -0,0 +1,11 @@
+ ## Checking a prime number 
+function Pcheck(number){
+ let count==0
+for(let i=1;i<num;i++){
+if(num/i==0){
+count++}}
+if(count==2){
+return true}
+return falsee}
+
+let check=Pcheck(13)
