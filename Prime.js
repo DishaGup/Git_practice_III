5a51df48ff580e36af67b52c20fe8e7ac673446f
agarwalshantanu29 <agarwalshantanu29@gmail.com>
2022-11-02 13:05:04 +0530

    Final Fix

--- Prime.js
+++ Prime.js
@@ -1,11 +1,27 @@
- //Checking a prime number 
-function Pcheck(number){
- let count==0;
-for(let i=1;i<number;i++){
-if(num/i==0){
-count++}}
-if(count==2){
-return true}
-return fals}
-
+//Checking a prime number 
+function Pcheck(number)
+{
+let count==0;
+for(let i=1;i<=number;i++)
+{
+if(num%i==0)
+{
+count++;
+}
+}
+if(count==2)
+{
+return true;
+}
+return false;
+}
+}
 let check=Pcheck(13)
+if(check==true)
+{
+console.log("Number is Prime");
+}
+else
+{
+console.log("Number is Not Prime");
+}
